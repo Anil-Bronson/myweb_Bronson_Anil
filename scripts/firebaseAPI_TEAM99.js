4c126c2940eb47074ef063b74ae4d7adeddfc6ff
Anil Bronson <anilbronson12@gmail.com>
2024-02-20 10:33:27 -0800

    added demo stuff

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,15 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyD1JxJ_xFvSIvQga9_M4oIMZ4jBEaDBICY",
+    authDomain: "comp1800-demo-2024.firebaseapp.com",
+    projectId: "comp1800-demo-2024",
+    storageBucket: "comp1800-demo-2024.appspot.com",
+    messagingSenderId: "895085989263",
+    appId: "1:895085989263:web:3a3cccbd81e63af03fcffa",
+    measurementId: "G-QM2XX31J1K"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
